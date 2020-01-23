384b29146285f9b52c528a64ffdf5a2f02d63ff7
jngmnghn <jngmnghn@jngmnghnui-MacBookPro.local>
2020-01-23 18:06:04 +0900

    백엔드 시작, sequelize 로 User table 생성

---
+++ back/models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+        'User',
+        {
+            nickname: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+            },
+            userId: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+        },
+        {
+            charset: 'utf8',
+            collate: 'utf8_general_ci',
+        },
+    );
+
+    User.associate = (db) => {
+        db.User.hasMany(db.Post);
+        db.User.hasMany(db.Comment);
+    };
+
+    return User;
+};
